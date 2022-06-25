7b7cb44b9f38d6804a026687dbaaa0682d1261bc
Kateryna Yakovleva <51397341+katyayakovleva@users.noreply.github.com>
2022-06-26 00:17:37 +0300

    commit

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="katya2905", auth_plugin='mysql_native_password')
+mydb = mysql.connector.connect(host="localhost", user="root", port="3307", passwd="katya2905", auth_plugin='mysql_native_password')
 
 my_cursor = mydb.cursor()
 
